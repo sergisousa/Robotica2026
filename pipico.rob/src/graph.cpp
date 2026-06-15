a881b6204b979d8ba26d6cf25d384ae2b615ce61
TcGamer6471 <tcgamer6471@gmail.com>
2026-06-15 01:21:41 +0100

    A* algorithm calculations
    implemented the A* algorithm

---
+++ pipico.rob/src/graph.cpp
@@ -0,0 +1,177 @@
+#include "Arduino.h"
+#include "graph.h"
+#include <cfloat>
+
+void selection_sort(const int size, int idx[], float array[]) {
+    // Create index array
+    for (int i = 0; i < size; i++) {idx[i] = i;}
+
+    // Variables needed for algorithm
+    float temp1, temp2;
+    int temp_idx;
+
+    // Sort using the selection sort algorithm
+    for (int i = 0; i < size - 1; i++) {
+        // Find index for smallest value
+        int min_idx = i;
+        for (int j = i + 1; j < size; j++) {
+            temp1 = array[idx[j]];
+            temp2 = array[idx[min_idx]];
+            if (temp1 < temp2) {
+                min_idx = j;
+            }
+        }
+        // Swap with current element
+        temp_idx = idx[min_idx];
+        idx[min_idx] = idx[i];
+        idx[i] = temp_idx;
+    }
+}
+
+int find_node_idx_by_label(int label) {
+    for (int i = 0; i < N_nodes; i++) {
+        if (node_labels[i] == label) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void obtain_adj_matrix(float matrix[N_nodes][N_nodes]) {
+    // make every value zero
+    for (int i = 0; i < N_nodes; i++) {
+        for (int j = 0; j < N_nodes; j++) {
+            matrix[i][j] = 0.0;
+        }
+    }
+    // insert cost of each connection
+    int idx_current_conn[MAX_connections];
+    for (int i = 0; i < N_nodes; i++) {
+        // translate labels to indices in connections
+        for (int idx = 0; idx < MAX_connections; idx++) {
+            if (node_conn[i][idx] != -1) {
+                idx_current_conn[idx] = find_node_idx_by_label(node_conn[i][idx]);
+            } else {
+                idx_current_conn[idx] = -1;
+            }
+        }
+        // go through all the other nodes
+        for (int j = i + 1; j < N_nodes; j++) {
+            // if there is a connection, insert the cost into the matrix
+            for (int idx = 0; idx < MAX_connections; idx++) {
+                if (idx_current_conn[idx] == j) {
+                    float cost = opt_cost_to_go(node_coords[i], node_coords[j]); // cost is the length + we can add other contributions
+                    matrix[i][j] = cost;
+                    matrix[j][i] = cost;
+                }
+            }
+        }
+    }
+}
+
+float opt_cost_to_go(const float node_coord[2], const float stop_node_coord[2]) {
+    float dist_x = node_coord[0] - stop_node_coord[0];
+    float dist_y = node_coord[1] - stop_node_coord[1];
+    return sqrt(dist_x * dist_x + dist_y * dist_y);
+}
+
+int is_array_zero(const int size, const int array[]) {
+    int result = 1;
+    for (int i = 0; i < size; i++) {
+        if (array[i] != 0) {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+int a_star(const int start_idx, const int stop_idx, int final_path[N_nodes]) {
+    // obtain adjacency matrix from connections (calculating the matrix for now, because its easier to change node connections, and when everything is set we use a defined matrix)
+    float node_adj_matrix[N_nodes][N_nodes];
+    obtain_adj_matrix(node_adj_matrix);
+
+    // define auxiliary arrays
+    int open_nodes[N_nodes];       // keep track of which nodes to explore next
+    int closed_nodes[N_nodes];     // keep track of which nodes have already been explored
+    float past_cost[N_nodes];      // cost of each node's best path
+    float est_total_cost[N_nodes]; // cost of each node's best path + estimated cost to go to goal
+    int node_parent[N_nodes];      // parent of node in the current best path
+    for (int i = 0; i < N_nodes; i++) {
+        open_nodes[i] = 0;           // 0 to ignore node, 1 to explore node
+        closed_nodes[i] = 0;         // 0 for open node,  1 for closed node
+        past_cost[i] = FLT_MAX;      // infinite cost for not-explored nodes
+        est_total_cost[i] = FLT_MAX; // infinite cost for not-explored nodes
+        node_parent[i] = -1;         // -1 for no parent
+        final_path[i] = -1;          // -1 for empty entry
+    }
+
+    // define start node
+    int current_idx = start_idx;
+    past_cost[current_idx] = 0;
+    open_nodes[current_idx] = 1;
+    est_total_cost[current_idx] = past_cost[current_idx] + opt_cost_to_go(node_coords[current_idx], node_coords[stop_idx]);
+
+    // algorithm logic
+    while (!is_array_zero(N_nodes, open_nodes)) {
+        // end the search if we are arrive in the stop node
+        if (current_idx == stop_idx) {
+            // obtain the amount of nodes in final path
+            int size_final = 0;
+            int idx = stop_idx;
+            while (idx != start_idx) {
+                idx = node_parent[idx];
+                size_final += 1;
+            }
+            // insert nodes in final_path in order of start node -> stop node
+            idx = stop_idx;
+            int node_pos = 0;
+            while (idx != start_idx) {
+                final_path[size_final - node_pos] = idx;
+                idx = node_parent[idx];
+                node_pos += 1;
+            }
+            final_path[size_final - node_pos] = idx;
+            return 1;
+        }
+
+        // find neighbors of current node and explore
+        for (int nbr_idx = 0; nbr_idx < N_nodes; nbr_idx++) {
+            // needs to be connected to current node, and not closed
+            if ((node_adj_matrix[current_idx][nbr_idx] > 0.0) && (closed_nodes[nbr_idx] == 0)) {
+                // if the neighbor node has no parent, update its cost and parent to the current node
+                if (node_parent[nbr_idx] == -1) {
+                    past_cost[nbr_idx] = past_cost[current_idx] + node_adj_matrix[current_idx][nbr_idx];
+                    node_parent[nbr_idx] = current_idx;
+                    open_nodes[nbr_idx] = 1;
+                    est_total_cost[nbr_idx] = past_cost[nbr_idx] + opt_cost_to_go(node_coords[nbr_idx], node_coords[stop_idx]);
+                } else {
+                    // if the total cost to go to the neighbor node, passing through the current node is better than the previous best path to the neighbor node
+                    // then set the new best path to the one going through the current node
+                    float new_cost = past_cost[current_idx] + node_adj_matrix[current_idx][nbr_idx];
+                    if (new_cost < past_cost[nbr_idx]) {
+                        past_cost[nbr_idx] = new_cost;
+                        node_parent[nbr_idx] = current_idx;
+                        open_nodes[nbr_idx] = 1;
+                        est_total_cost[nbr_idx] = past_cost[nbr_idx] + opt_cost_to_go(node_coords[nbr_idx], node_coords[stop_idx]);
+                    }
+                }
+            } 
+        }
+        
+        // close current node
+        closed_nodes[current_idx] = 1;
+        open_nodes[current_idx] = 0;
+        // sort the nodes by lowest estimated total cost
+        int sorted_idx[N_nodes];
+        selection_sort(N_nodes, sorted_idx, est_total_cost);
+        // select the first node that is not closed
+        for (int i = 0; i < N_nodes; i++) {
+            if (closed_nodes[sorted_idx[i]] == 0) {
+                current_idx = sorted_idx[i];
+                break;
+            }
+        }
+    }
+    return 0; // failed to find a path
+}
+
---
+++ pipico.rob/src/graph.h
@@ -0,0 +1,74 @@
+#ifndef graph_h
+#define graph_h
+
+#define N_nodes 27
+#define MAX_connections 4
+
+// define node connections
+int node_conn[N_nodes][MAX_connections] = {
+    // -1 means no connection
+    {30,  6, -1, -1}, // Node 4
+    { 9, 31, -1, -1}, // Node 5
+    { 4, 13, 14, 30}, // Node 6
+    {13, 42, -1, -1}, // Node 7
+    { 9, 41, -1, -1}, // Node 8
+    {16,  8, 31,  5}, // Node 9
+    { 6,  7, 20, -1}, // Node 13
+    { 6, 40, -1, -1}, // Node 14
+    {16, 43, -1, -1}, // Node 15
+    { 9, 35, 15, -1}, // Node 16
+    {23, 22, 13, 35}, // Node 20
+    {20, 23, -1, -1}, // Node 22
+    {20, 22, 24, 36}, // Node 23
+    {23, 25, 37, -1}, // Node 24
+    {24, 26, 35, 38}, // Node 25
+    {25, 35, 39, -1}, // Node 26
+    { 4, 31,  6, -1}, // Node 30
+    {30,  5,  9, -1}, // Node 31
+    {16, 20, 25, 26}, // Node 35
+    {23, -1, -1, -1}, // Node 36
+    {24, -1, -1, -1}, // Node 37
+    {25, -1, -1, -1}, // Node 38
+    {26, -1, -1, -1}, // Node 39
+    {14, -1, -1, -1}, // Node 40
+    { 8, -1, -1, -1}, // Node 41
+    { 7, -1, -1, -1}, // Node 42
+    {15, -1, -1, -1}  // Node 43
+};
+
+// define coordinates {x,y} of each node
+const float node_coords[N_nodes][2] = {
+    {  0.0f,  0.355f}, // Node 4
+    {0.695f,  0.355f}, // Node 5
+    {  0.0f,   0.15f}, // Node 6
+    {  0.2f,    0.0f}, // Node 7
+    {  0.5f,   0.15f}, // Node 8
+    {0.695f,   0.15f}, // Node 9
+    {  0.0f,    0.0f}, // Node 13
+    {  0.2f,   0.15f}, // Node 14
+    {  0.5f,    0.0f}, // Node 15
+    {0.695f,    0.0f}, // Node 16
+    {  0.0f,  -0.15f}, // Node 20
+    {  0.0f, -0.355f}, // Node 22
+    {0.227f,   0.15f}, // Node 40
+    {0.468f,   0.15f}, // Node 41
+    {0.227f,    0.0f}, // Node 42
+    {0.468f,    0.0f}, // Node 43
+    {0.245f, -0.355f}, // Node 23
+    {0.395f, -0.355f}, // Node 24
+    {0.545f, -0.355f}, // Node 25
+    {0.695f, -0.355f}, // Node 26
+    {0.245f, -0.385f}, // Node 36
+    {0.395f, -0.385f}, // Node 37
+    {0.545f, -0.385f}, // Node 38
+    {0.695f, -0.385f}, // Node 39
+    {0.227f,  0.355f}, // Node 30
+    {0.468f,  0.355f}, // Node 31
+    {0.695f,  -0.15f}, // Node 35
+};
+
+const int node_labels[N_nodes] = {4, 5, 6, 7, 8, 9, 13, 14, 15, 16, 20, 22, 40, 41, 42, 43, 23, 24, 25, 26, 36, 37, 38, 39, 30, 31, 35};
+
+int a_star(const int start_idx, const int stop_idx, int final_path[N_nodes]);
+
+#endif
